17f8d24b39bbb00c39eacc3f9d41945ff0cf4d11
GITHUB <35479799+ge1hub@users.noreply.github.com>
2020-09-21 12:40:27 +0800

    Update index.js

--- qr/js/index.js
+++ qr/js/index.js
@@ -31,20 +31,7 @@ $(function () {
 			},
 		});
 
-		// 弹窗广告,不需要则删除该段代码
-		layer.open({
-			type: 1,
-			area: ['310px', '450px'], //宽高
-			content: '<div style="text-align: center;"><br><img src="https://imgs.bwmelon.com/20190722123548.png" style="width:80%;margin-bottom:15px;"><br>"扫码二维码查看支付效果或者打赏"<br>"本站生成的收款码无红包广告"<br>"又拍云CDN加速，收款更快捷"</div>',
-			title: '支付宝扫码领红包',
-			btn: ['关闭'],
-			btnAlign: 'c',
-			shade: 0,
-			btn1: function () {
-				layer.closeAll();
-			}
-		});
-
+		
 		/**
 		 * url编码
 		 * @param {String} String 
